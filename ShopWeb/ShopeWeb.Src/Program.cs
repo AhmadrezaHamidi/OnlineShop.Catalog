44abba7bc8a7b45a7232c59018c8ee05605fa0b2
ahmadhfr <vestaabner@gmail.com>
2023-05-28 11:10:43 +0330

    Feat add ...

---
+++ ShopWeb/ShopeWeb.Src/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection.Metadata;
+using Microsoft.EntityFrameworkCore;
+using ShopeWeb.Src;
+using ShopWeb.Base.PagedList.UnitOfWork;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddDbContext<ShopDbCobtext>(opt => opt.UseSqlServer("Server=127.0.0.1,1433;Database=Ahmad;User Id=sa;Password=yourStrong(!)Password;MultipleActiveResultSets=True;"))
+//.AddDbContext<BloggingContext>(opt => opt.UseInMemoryDatabase("UnitOfWork"))
+                .AddUnitOfWork<ShopDbCobtext>();
+                //.AddCustomRepository<Blog, CreateBuilderustomBlogRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
---
+++ ShopWeb/ShopeWeb.Src/ShopContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace ShopeWeb.Src
+{
+    public class ShopDbCobtext : DbContext
+    {
+        public ShopDbCobtext(DbContextOptions<ShopDbCobtext> options)
+            : base(options)
+        { }
+
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.EnableAutoHistory(null);
+        }
+    }
+
+    public class Blog
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public string Title { get; set; }
+
+        public List<Post> Posts { get; set; }
+    }
+
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public List<Comment> Comments { get; set; }
+    }
+
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
+
